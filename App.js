fd7b6a91f9118184da95e703b849d944bc9c692a
abughalib <abughalib64@gmail.com>
2020-08-11 09:32:42 +0530

    react-native-flex

--- App.js
+++ App.js
@@ -1,21 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () =>{
+	return(
+		<view style={{
+			paddingLeft:10,
+			paddingRight: 10,
+		}}>
+		<View style={{
+			flex:1,
+			flexDirection: 'row',
+			justifyContent: 'space-between',
+		}}>
+		<View style={{
+			flexDirection: 'row',
+			justifyContent: 'flex-start',
+		}}>
+		<View>
+		<Text style={{
+			height: 50,
+			fontSize: 25,
+			backgroundColor: 'red'
+		}}>Abu Ghalib</Text>
+		</View>
+		<View>
+		<Text style={{
+			height: 50,
+			fontSize: 25,
+			backgroundColor: 'green'
+		}}>First</Text>
+		</View>
+		<View>
+		<Text style={{
+			height: 50,
+			fontSize: 25,
+			backgroundColor: 'yellow'
+		}}>Second</Text>
+		</View>
+		
+		</View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+		<View style={{
+			flexDirection: 'row',
+			justifyContent: 'flex-end',
+		}}>
+		<View>
+		<Text style={{
+			height: 50,
+			fontSize: 25,
+			backgroundColor: 'red'
+		}}>Abugh</Text>
+		</View>
+		<View>
+		<Text style={{
+			height: 50,
+			fontSize: 25,
+			backgroundColor: 'green'
+		}}>First</Text>
+		</View>
+		<View>
+		<Text style={{
+			height: 50,
+			fontSize: 25,
+			backgroundColor: 'yellow'
+		}}>Second</Text>
+		</View>
+		</View>
+
+		</View>
+
+		<View style={{
+			height:100,
+			flex: 1,
+			justifyContent: "center",
+			
+		}}>
+		<Text style={{
+			fontSize: 40,
+		}}>Hi There I'm Abu Ghalib</Text>
+
+		<Button style={{
+			height: 20,
+			width: 40,
+		}}>Hello</Button>
+
+		</View>
+
+		</view>
+	);
+};
+
+export default App;
